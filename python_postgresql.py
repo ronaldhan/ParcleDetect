257fc251f6e110df455349251c242a7c1306d543
ronaldhan <ronald.han7@gmail.com>
2014-12-23 11:23:18 +0800

    new functions

--- python_postgresql.py
+++ python_postgresql.py
@@ -8,6 +8,8 @@ class Connection(object):
     def __init__(self, host, database, user=None, password=None, port=5432):
         self.host = host
         self.database = database
+        args = dict(database=database, user=user, password=password, host=host, port=port)
+        self._args = args
 
         try:
             self.conn = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
@@ -31,6 +33,10 @@ class Connection(object):
                 self.conn.rollback()
                 logging.exception("Can not commit", e)
 
+    def reconnect(self):
+        self.conn.close()
+        self.conn = psycopg2.connect(**self._args)
+
     def rollback(self):
         if self.conn is not None:
             try:
@@ -38,4 +44,21 @@ class Connection(object):
             except Exception, e:
                 logging.error("Can not rollback")
 
+    def _cursor(self):
+        if self.conn is None: self.reconnect()
+        return self.conn.cursor()
 
+    def execute(self, query, *parameters):
+        cursor = self._cursor()
+        try:
+            self._execute(cursor, query, parameters)
+            return cursor.lastrowid
+        finally:
+            cursor.close()
+    def _execute(self, cursor, query, parameters):
+        try:
+            return cursor.execute(query, parameters)
+        except OperationalError:
+            logging.error("Error connecting to MySQL on %s", self.host)
+            self.close()
+            raise
